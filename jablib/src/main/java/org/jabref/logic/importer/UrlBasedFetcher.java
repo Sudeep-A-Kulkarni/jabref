6991fe83d45d8e2939b8aa722cf7fbcb6bead063
sudeep <kulkarnisudeep2005@gmail.com>
2026-04-05 14:21:37 +0530

    Update UrlBasedFetcher.java

--- jablib/src/main/java/org/jabref/logic/importer/UrlBasedFetcher.java
+++ jablib/src/main/java/org/jabref/logic/importer/UrlBasedFetcher.java
@@ -2,7 +2,6 @@ package org.jabref.logic.importer;
 
 import java.util.List;
 
-import org.jabref.logic.importer.FetcherException;
 import org.jabref.model.entry.BibEntry;
 
 public interface UrlBasedFetcher extends WebFetcher {
